17e16d7d608ce90138e948e9d9574b0d8d8041e2
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2022-04-11 11:50:24 +0530

    CRUD complete. exercises 3.1-3.6

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const { reset } = require('nodemon')
 const app = express()
 const data = require('./db.json')
 
+//json-parser
+app.use(express.json())
+
+// root
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook Backend</h1>')
 })
 
+// GET all
 app.get('/api/persons', (request, response) => {
   response.json(data)
 })
 
+// info
 app.get('/info', (request, response) => {
   response.send(
     `Phonebook has info for ${
@@ -20,6 +26,7 @@ app.get('/info', (request, response) => {
   response.send(``)
 })
 
+// GET particular
 app.get('/api/persons/:id', (request, response) => {
   const id = +request.params.id
   const thePerson = data.persons.find((person) => person.id === id)
@@ -31,6 +38,7 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+// DELETE
 app.delete('/api/persons/:id', (request, response) => {
   const id = +request.params.id
   const found = data.persons.some((person) => person.id === id)
@@ -39,6 +47,38 @@ app.delete('/api/persons/:id', (request, response) => {
     response.send('Person deleted from database')
   } else {
     response.send('Person to be deleted not found in the database')
+  }
+})
+
+// Generate new id for new entries
+const generateId = () => {
+  const randomId = (Math.random() * 10000000).toFixed(0)
+  return randomId
+}
+
+//CREATE
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (body.name && body.phone) {
+    if (
+      data.persons.some(
+        (person) => person.name.toLowerCase() === body.name.trim().toLowerCase()
+      )
+    ) {
+      response.send(`Person with the name ${body.name} already exists`)
+    } else {
+      const newPerson = {
+        id: generateId(),
+        name: body.name,
+        phone: body.phone,
+      }
+
+      data.persons = data.persons.concat(newPerson)
+      response.send(`Person added <br /> ${JSON.stringify(data)}`)
+    }
+  } else {
+    response.send('please enter name as well as phone')
   }
 })
 
